2591db0ae3ba37a0b2a1f80cf194c7aa90d34147
E2173hasanfuat <69896039+E2173hasanfuat@users.noreply.github.com>
2021-07-14 00:06:10 +0200

    element secme ve eventlistenerlar
    function icinde function

---
+++ todo.js
@@ -0,0 +1,17 @@
+// tum elementler secme
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+eventListener();
+
+function eventListener(){//tum eevent listenerleri
+    form.addEventListener("submit",addTodo);
+}
+function addTodo(e){
+
+}

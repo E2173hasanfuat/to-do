02db9b7c8189917898734b25b3e5418ada14e03b
E2173hasanfuat <69896039+E2173hasanfuat@users.noreply.github.com>
2021-07-14 10:39:00 +0200

    g

--- todo.js
+++ todo.js
@@ -13,5 +13,46 @@ function eventListener(){//tum eevent listenerleri
     form.addEventListener("submit",addTodo);
 }
 function addTodo(e){
+    const newTodo = todoInput.value.trim();
+    
+    addTodoToUI (newTodo);
 
+
+
+
+
+    e.preventDefault();
+}
+function addTodoToUI(newTodo){
+    //string degerini list item olarak UI yi ekleyecek
+// <!-- <li class="list-group-item d-flex justify-content-between">
+// Todo 1
+// <a href = "#" class ="delete-item">
+//     <i class = "fa fa-remove"></i>
+// </a>
+
+// </li>-->
+
+//list item olusturma
+const listItem = document.createElement("li");
+//link olusturma
+const link = document.createElement("a");
+link.href = "#";
+link.className = "delete-item";
+link.innerHTML = "<i class = 'fa fa-remove'></i>";
+
+listItem.className = "list-group-item d-flex justify-content-between"
+
+//Text  Node ekleme
+
+listItem.appendChild(document.createTextNode(newTodo));
+listItem.appendChild(link);
+
+//Todo List'e LIst Item i ekleme
+
+todoList.appendChild(listItem);
+todoInput.value = "";
+
+
+console.log(listItem);
 }
